72c2905fc30cb65a734eb643f8b936c2837e79ad
ArifahZhafirah <arifahzhafirah206@gamil.com>
2026-04-20 21:59:27 +0700

    Membuat Resource Filament

---
+++ app/Filament/Admin/Resources/Barangs/BarangResource.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\Admin\Resources\Barangs;
+
+use App\Filament\Admin\Resources\Barangs\Pages\CreateBarang;
+use App\Filament\Admin\Resources\Barangs\Pages\EditBarang;
+use App\Filament\Admin\Resources\Barangs\Pages\ListBarangs;
+use App\Filament\Admin\Resources\Barangs\Schemas\BarangForm;
+use App\Filament\Admin\Resources\Barangs\Tables\BarangsTable;
+use App\Models\Barang;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+
+class BarangResource extends Resource
+{
+    protected static ?string $model = Barang::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    protected static ?string $recordTitleAttribute = 'Barang';
+
+    public static function form(Schema $schema): Schema
+    {
+        return BarangForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return BarangsTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListBarangs::route('/'),
+            'create' => CreateBarang::route('/create'),
+            'edit' => EditBarang::route('/{record}/edit'),
+        ];
+    }
+}
